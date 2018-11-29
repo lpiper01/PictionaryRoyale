33a606ec5b63a646f6437d084b7692066862fb78
Louis Piper Carson <louis.pipercarson@gmail.com>
2018-11-29 02:22:03 -0500

    Revert "some merging stuff"
    This reverts commit 4968836dd43b6a224b8cf13d8a06b0a7269d464a, reversing
    changes made to 5f858043133ab4d8c9d50d15ef78b071f342a81f.

--- client/client.py
+++ client/client.py
@@ -15,16 +15,9 @@ PANELSIZE = (300, 300)
 DEBUG = True
 CLIENT_NAME = 'CLIENT'
 FPS_LIMIT = 100
-<<<<<<< HEAD
 LINE_DIVISOR = 10
 LINES_INDEX = 1
 PANEL_INDEX = 0
-=======
-
-PANEL_INDEX = 0
-LINES_INDEX = 1
-
->>>>>>> ede38edd720da9859d4c65fadcd5efa153a6c9e1
 # TODO:
 # - resizable?
 def rel_to_abs(rel_x, rel_y, gap = 10):
@@ -85,19 +78,16 @@ class App:
         self.running = False
 
     def _startline(self, params):
-        """Begins a line in a given panel at pos"""
         panel_id = params[0]
         pos = params[1]
         self.client.startline(panel_id, pos)
 
     def _endline(self, params):
-        """Ends a line in a given panel, terminating at pos"""
         panel_id = params[0]
         pos = params[1]
         self.client.endline(panel_id, pos)
 
     def _addpoint(self, params):
-        """Adds a point to the most recently begun line in a panel at pos"""
         panel_id = params[0]
         pos = params[1]
         self.client.addpoint(panel_id, pos)
@@ -121,7 +111,6 @@ class Client:
         self.clock = pygame.time.Clock()
 
         # Client's canvas + chat
-        # TODO: Add chat to the panels dictionary (should be treated the same as any other panel)
         panel_location = rel_to_abs(0, 0)
         chat_location = rel_to_abs(1, 0)
         self.chat_panel = Chat(chat_location, PANELSIZE, self.screen)
@@ -154,12 +143,19 @@ class Client:
     def add_panel(self, panel_id):
         """Add a panel with the given ID to the Client.
         """
-        pass
+        new_panel = Panel(panel_id)
+        other_panels.append(new_panel)
 
     def remove_panel(self, panel_id):
         """Remove the panel with given ID.
         """
-        pass
+        # WOULD PROBABLY BREAK STUFF RIGHT NOW
+        index = None
+        for current, panel in enumerate(self.other_panels):
+            if panel.getID() == panel_id:
+                index = current;
+
+        self.other_panels.pop(index)
 
     def process(self):
         """Draw updates to screen, handle events
@@ -175,25 +171,16 @@ class Client:
     def endline(self, panel_id, pos):
         """Ends the last line on the given panel"""
         target_panel = self.panels[panel_id]
-<<<<<<< HEAD
         target_linenum = len(target_panel[LINES_INDEX]) - 1
         target_line = target_panel[LINES_INDEX][target_linenum]
         target_line.append(pos)
         target_line = target_line[0::LINE_DIVISOR]
-=======
-        target_line = len(target_panel[LINES_INDEX]) - 1
-        target_panel[LINES_INDEX][target_line].append(pos)
-        target_panel[LINES_INDEX][target_line] = target_panel[LINES_INDEX][target_line][0::10]
->>>>>>> ede38edd720da9859d4c65fadcd5efa153a6c9e1
 
     def addpoint(self, panel_id, pos):
         """Adds a point to the last line on the given panel"""
         target_panel = self.panels[panel_id]
         target_line = len(target_panel[LINES_INDEX]) - 1
-<<<<<<< HEAD
         print "hi"
-=======
->>>>>>> ede38edd720da9859d4c65fadcd5efa153a6c9e1
         target_panel[LINES_INDEX][target_line].append(pos)
 
     def _draw(self):
